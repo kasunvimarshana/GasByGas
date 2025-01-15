344b852407ad88b98033d864b3e88339dc3dc8bb
kasunvimarshana <kasunvmail@gmail.com>
2025-01-15 14:15:02 +0530

    setup 'spatie/laravel-permission'

--- src/config/app.php
+++ src/config/app.php
@@ -170,6 +170,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Barryvdh\Debugbar\ServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
     ])->toArray(),
 
     /*
