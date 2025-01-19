ce6b0e90074755c9c6ac4e7817461fe58647f30a
kasunvimarshana <kasunvmail@gmail.com>
2025-01-19 22:08:04 +0530

    Edit: 'HasCompanyScope'

--- src/app/Traits/HasCompanyScope.php
+++ src/app/Traits/HasCompanyScope.php
@@ -9,6 +9,7 @@ trait HasCompanyScope {
         return 'company_id';
     }
 
+    // Boot the trait and add the global scope to the model
     protected static function bootHasCompanyScope() {
         $scope_column = static::getCompanyScopeColumn();
         static::addGlobalScope(new CompanyScope($scope_column));
