113efbea7bbbd70fd79d28d0b18acbbaff81637d
kasunvimarshana <kasunvmail@gmail.com>
2025-01-20 01:44:23 +0530

    php artisan make:model Stock -a php artisan make:model StockMovement -a php artisan make:model Order -a php artisan make:model OrderItem -a php artisan make:model Purchase -a php artisan make:model PurchaseItem -a php artisan make:model ItemReturn -a php artisan make:model ReturnItem -a php artisan make:model Delivery -a php artisan make:model Payment -a

---
+++ src/app/Http/Controllers/ItemReturnController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreItemReturnRequest;
+use App\Http\Requests\UpdateItemReturnRequest;
+use App\Models\ItemReturn;
+
+class ItemReturnController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreItemReturnRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ItemReturn $itemReturn)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ItemReturn $itemReturn)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateItemReturnRequest $request, ItemReturn $itemReturn)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ItemReturn $itemReturn)
+    {
+        //
+    }
+}
