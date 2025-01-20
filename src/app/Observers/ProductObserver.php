8d76492d10c76f316393d5c49033cd873459b7c7
kasunvimarshana <kasunvmail@gmail.com>
2025-01-20 17:22:14 +0530

    Edit: 'ProductObserver'

--- src/app/Observers/ProductObserver.php
+++ src/app/Observers/ProductObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use Illuminate\Support\Facades\Log;
+// use Illuminate\Support\Str;
 use Exception;
 use App\Models\Company;
 use App\Models\Product;
@@ -21,5 +22,23 @@ class ProductObserver {
         } catch (Exception $e) {
             Log::error('Error creating product: ' . $e->getMessage());
         }
+    }
+
+    public function saving(Product $model): void {
+        // $model->forceFill([]);
+        if (empty($model->sku)) {
+            // Retrieve the maximum ID from the database
+            $lastProductId = Product::max('id') ?? 0;
+
+            $generatedSKU = $this->generateSKU($lastProductId);
+
+            // Set the generated sku to the model
+            $model->sku = $generatedSKU;
+        }
+    }
+
+    private function generateSKU(int $lastProductId): string {
+        $newProductId = $lastProductId + 1;
+        return 'SKU_' . str_pad($newProductId, 5, '0', STR_PAD_LEFT);
     }
 }
